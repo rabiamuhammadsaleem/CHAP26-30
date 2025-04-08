0fcd7917d647941f0ae5abff5aaa1add030bb408
rabiamuhammadsaleem <rabiasaleem2307@gmail.com>
2025-04-08 21:27:51 +0500

    commit

---
+++ chap 26-30/app.js
@@ -0,0 +1,104 @@
+    //    ----------- Chapter 26-30 Maths Methods ----------
+   
+       "QUESTION NO:01"
+// 1.Write a program that takes a positive integer from user & display the following in your browser 
+// a. number 
+// b. round off value of the number 
+// c. floor value of the number
+// d. ceil value of the number.
+
+// let num1 = +prompt("Enter possitive number: ");
+// document.write("Number: "+num1+"<br>")
+
+// let round1 = Math.round(num1);
+// document.write("Round off value: "+round1+"<br>")
+
+// let floor1 = Math.floor(num1);
+// document.write("Floor value: "+floor1+"<br>")
+
+// let ceil1 = Math.ceil(num1);
+// document.write("ceil value: "+ceil1+"<br>")
+
+
+       "QUESTION NO:02"
+// 2.Write a program that takes a negative floating point number from user & display the following in your browser.
+
+// a. number 
+// let num2 = +prompt("Enter negative number: ");
+// document.write("Number: "+num2+"<br>")
+
+// b. round off value of the number
+// let round2 = Math.round(num2);
+// document.write("Round off value: "+round2+"<br>")
+
+// c. floor value of the number
+// let floor2 = Math.floor(num2);
+// document.write("Floor value: "+floor2+"<br>")
+
+// d. ceil value of the number
+// let ceil2 = Math.ceil(num2);
+// document.write("ceil value: "+ceil2+"<br>")
+
+
+        "QUESTION NO:03"
+// 3.Write a program that displays the absolute value of a number. 
+
+// let number = +prompt("Enter number :");
+// let absolute = Math.abs(number);
+// document.write("The absolute value of "+number+"  is "+absolute)         
+       
+
+          "QUESTION NO:04"
+// 4.Write a program that simulates a dice using random() method of JS Math class.Display the value of dice in your browser.let
+
+// let randomNum = Math.random()*6;
+// let floorNum = Math.floor(randomNum);
+// document.write("random die value is  : "+floorNum) 
+
+
+           "QUESTION NO:05"
+// 5.Write a program that simulates a coin toss using random() method of JS Math class.Display the value of coin in your browser 
+ 
+// let coinToss = Math.random()*2;
+// let floorNo = Math.floor(coinToss);
+// if(coinToss<1){
+//     document.write(floorNo+"<br>"+"" Random coin toss is : Tail")
+// }
+// else{
+//     document.write(floorNo+"<br>"+"" Random coin toss is : Head")
+// }           
+
+
+          "QUESTION NO:06"
+// 6.Write a program that shows a random number between 1 and 100 in your browser. 
+
+// let random = Math.random()*100;
+// let ceil = Math.ceil(random);
+// document.write("Random number between 1 and 100 is :"+ceil) 
+
+
+           "QUESTION NO:07"
+// 7.Write a program that asks the user about his weight.Parse the user input and display his weight in your browser
+
+// let weight = prompt("Enter your weight in kilograms");
+// let parse = parseFloat(weight);
+// document.write("The weight of user is "+parse.toFixed(1)+" kilograms")
+
+
+          "QUESTION NO:08"
+// 8.Write a program that stores a random secret number from 1 to 10 in a variable.Ask the user to input a number between 1 and 10.
+// If the user input equals the secret number, congratulate the user. 
+
+           "ANSWER NO:08"
+// let randomNo = Math.ceil((Math.random()*10));
+// document.write(randomNo)
+// let userNo = prompt("Enter a number between 1 and 10");
+// if(userNo === randomNo){
+//     alert("Congratulations you entered the same number")
+// }
+// else{
+//     alert("Try Again")
+// }          
+
+                
+            //    ----------- Chapter 26-30 Complete ----------
